2e58cca9f17ce0e8cd8c4e04c47c4d3c41ad7183
computer <stefan.nik04@gmail.com>
2024-11-11 20:23:09 +0100

    feat add recycle view for merchandise and events

--- app/src/main/java/com/example/zadatak2/HomeScreen.java
+++ app/src/main/java/com/example/zadatak2/HomeScreen.java
@@ -2,6 +2,7 @@ package com.example.zadatak2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,13 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.zadatak2.databinding.ActivityHomeScreenBinding;
+import com.example.zadatak2.event.Event;
+import com.example.zadatak2.event.EventViewModel;
+import com.example.zadatak2.event.EventsList;
+import com.example.zadatak2.merchandise.Merchandise;
+import com.example.zadatak2.merchandise.MerchandiseList;
+import com.example.zadatak2.merchandise.MerchandiseViewModel;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HomeScreen extends AppCompatActivity {
-
+    private MerchandiseViewModel merchandiseViewModel;
+    private EventViewModel eventViewModel;
     ActivityHomeScreenBinding activityHomeScreenBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,23 @@ public class HomeScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        List<EventMerchandise> eventMerchandiseList=new ArrayList<>();
+        merchandiseViewModel=new ViewModelProvider(this).get(MerchandiseViewModel.class);
+        eventViewModel=new ViewModelProvider(this).get(EventViewModel.class);
+        for (Merchandise merchandise:merchandiseViewModel.merchandises
+             ) {
+            eventMerchandiseList.add(new EventMerchandise(EventMerchandise.MERCHANDISE,merchandise));
+        }
+        for (Event event:eventViewModel.events
+        ) {
+            eventMerchandiseList.add(new EventMerchandise(EventMerchandise.EVENT,event));
+        }
+        EventsMerchandiseAdapter adapter= new EventsMerchandiseAdapter(eventMerchandiseList);
+        RecyclerView recyclerView = findViewById(R.id.events_merchandise_recycler_view);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
     @Override
     protected void onStart() {
