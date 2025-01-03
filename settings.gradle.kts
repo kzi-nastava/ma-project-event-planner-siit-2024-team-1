89db290becc897cfa51b23a69ccb347aab1f30ff
computer <stefan.nik04@gmail.com>
2025-01-03 14:51:42 +0100

    added notifications for events

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,16 +9,18 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Add this line
     }
 }
 
 rootProject.name = "EventPlanner"
 include(":app")
- 
