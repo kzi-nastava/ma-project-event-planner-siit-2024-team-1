e8cddb53e864dcad1b9e1bf92caa9fa44bc561db
computer <stefan.nik04@gmail.com>
2025-01-04 22:13:29 +0100

    made notifications clickable

--- app/src/main/java/com/example/EventPlanner/activities/ProductDetailsActivity.java
+++ app/src/main/java/com/example/EventPlanner/activities/ProductDetailsActivity.java
@@ -3,7 +3,6 @@ package com.example.EventPlanner.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,19 +11,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.EventPlanner.R;
-import com.example.EventPlanner.databinding.ActivityServiceDetailsBinding;
 
-public class ServiceDetailsActivity extends AppCompatActivity {
+public class ProductDetailsActivity extends AppCompatActivity {
+
     private int merchandiseId;
-    private ActivityServiceDetailsBinding activityServiceDetailsBinding;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        activityServiceDetailsBinding=ActivityServiceDetailsBinding.inflate(getLayoutInflater());
-        setContentView(activityServiceDetailsBinding.getRoot());
+        setContentView(R.layout.activity_product_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -32,15 +27,6 @@ public class ServiceDetailsActivity extends AppCompatActivity {
         });
         Intent intent=getIntent();
         merchandiseId=intent.getIntExtra("MERCHANDISE_ID",-1);
-        Button bookReservationBtn=activityServiceDetailsBinding.bookReservationBtn;
-        bookReservationBtn.setOnClickListener(v->openBookReservation());
         Log.d("merchandise_id", String.valueOf(merchandiseId));
-
-    }
-
-    private void openBookReservation(){
-        Intent intent = new Intent(ServiceDetailsActivity.this, BookReservationActivity.class);
-        intent.putExtra("MERCHANDISE_ID", merchandiseId);
-        startActivity(intent);
     }
 }
